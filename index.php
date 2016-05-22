dcce0b1a81f04f178a1d0182cf33faae50800354
Ismaïl <nguyen.ismail@gmail.com>
2016-05-22 23:35:40 +0200

    Remove poc

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 	if (isset($_GET['controller'])) {
 		$controller = $_GET['controller'];
-		
+
 		$action = isset($_GET['action'])
-					? $_GET['action']
-					: 'index';
+			? $_GET['action']
+			: 'index';
 	}
 	else {
 		$controller = 'pages';
